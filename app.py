a95780b8725b31f02ed3a36d7cfca1c08bbeb706
wmokrogulski <mokrogulski1b@gmail.com>
2021-10-18 23:42:43 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, make_response
+from calendar_utils import calendar_from_excel
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/response', methods=['GET', 'POST'])
+def response():
+    file=request.files['file']
+    if file.filename.split('.')[-1] not in ['xls', 'xlsx']:
+        return 'Invalid file type'
+    cal=calendar_from_excel(request.files['file'])
+    print(str(cal))
+
+    response = make_response(str(cal))
+    cd = 'attachment; filename=test.ics'
+    response.headers['Content-Disposition'] = cd
+    response.mimetype = 'text/plain'
+
+    return response
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ calendar_utils.py
@@ -0,0 +1,67 @@
+import datetime
+from datetime import timedelta
+
+import numpy as np
+import pandas as pd
+
+from ics import Calendar, Event
+import time
+
+
+def read_from_excel(filename='test.xlsx'):
+    df = pd.read_excel(filename)
+    events = []
+    for index, row in df.iterrows():
+        if not pd.isnull(row['koniec']):
+            row.fillna('', inplace=True)
+            e = create_event(row['nazwa'], row['początek'], end=row['koniec'], location=row['miejsce'],
+                             description=row['opis'])
+        else:
+            row.fillna('', inplace=True)
+            e = create_event(row['nazwa'], row['początek'],
+                             duration=timedelta(minutes=int(row['czas trwania (min)'])), location=row['miejsce'],
+                             description=row['opis'])
+        events.append(e)
+    return events
+
+
+def create_event(name, begin, duration=timedelta(hours=0.5), end=None, location=None, description=None):
+    event = Event()
+    event.name = name
+    event.begin = begin
+    if end is not None:
+        event.end = end
+    else:
+        event.duration = duration
+
+    if location is not None:
+        event.location = location
+
+    if description is not None:
+        event.description = description
+    return event
+
+
+def test_calendar():
+    cal = Calendar()
+    e = create_event('My wonderful event', datetime.datetime(2021, 10, 14, 11, 0), location='Chełmońskiego 9, Warszawa')
+    cal.events.add(e)
+    for event in read_from_excel():
+        cal.events.add(event)
+
+    print(cal)
+
+    with open('test.ics', 'w', encoding='utf-8') as f:
+        f.write(str(cal))
+
+
+def calendar_from_excel(filename):
+    cal = Calendar()
+    for event in read_from_excel(filename):
+        cal.events.add(event)
+
+    return cal
+
+
+if __name__ == '__main__':
+    test_calendar()
